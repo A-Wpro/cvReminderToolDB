02ba7e28e4a938a4250e458fd445dc6f38b0f352
Wecker <36549997+A-Wpro@users.noreply.github.com>
2020-03-09 14:55:23 +0100

    Update connexion.php
    deleted DB password

--- connexion.php
+++ connexion.php
@@ -11,8 +11,8 @@
     $hote = 'db648575668.db.1and1.com';
     $base = 'db648575668';
     $user = 'dbo648575668';
-    $pass = '1403Mj!<3';
+    $pass = '';
  
-    $cnx = mysqli_connect ("db648575668.db.1and1.com","dbo648575668","1403Mj!<3","db648575668") or die(mysqli_error());
+    $cnx = mysqli_connect ("db648575668.db.1and1.com","dbo648575668","","db648575668") or die(mysqli_error());
     $ret = mysqli_select_db($cnx,$base) or die(mysqli_error());
 ?>
