7684d9035d8e4d77efd3ce421315d0ed6067f022
Wecker <36549997+A-Wpro@users.noreply.github.com>
2020-03-09 14:57:08 +0100

    Update FonctionAjoutCV.php
    deleted DB PW

--- FonctionAjoutCV.php
+++ FonctionAjoutCV.php
@@ -8,7 +8,7 @@
 
 
             
-            $connect = mysqli_connect ("db648575668.db.1and1.com","dbo648575668","1403Mj!<3","db648575668");
+            $connect = mysqli_connect ("db648575668.db.1and1.com","dbo648575668","","db648575668");
                 if($connect){
                      
                     /* Mis en BDD des cv
